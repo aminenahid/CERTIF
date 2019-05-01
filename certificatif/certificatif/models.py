e4a101692ee5116b70a280e82900c9bc057b777e
manahid <aminenahid@gmail.com>
2019-05-01 10:45:31 +0200

    UPDATE : model changes (heritage from now on)

--- certificatif/certificatif/models.py
+++ certificatif/certificatif/models.py
@@ -1,25 +1,54 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 import datetime 
+from django.core import validators
 from django.contrib.postgres.fields import JSONField
+from django.utils.translation import ugettext_lazy as _
 
-class User(AbstractUser):
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=30, unique=True,
+        validators=[
+            validators.RegexValidator(r'^[\w.@+-]+$',
+                _('Enter a valid username. '
+                'This value may contain only letters, numbers '
+                'and @/./+/-/_ characters.'), 'invalid'),
+        ],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        })
     public_key = models.CharField(max_length=64, unique=True)
-    is_student = models.BooleanField(default=False)
-    is_university = models.BooleanField(default=False)
+    date_joined = models.DateField(default=datetime.date.today)
 
     USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email','public_key']
 
-class Student (models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Student (User):
     given_names = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
-class University (models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    def get_full_name(self):
+        return "%s %s" % (self.given_names, self.last_name)
+    
+    def get_short_name(self):
+        return "%s" % (self.given_names[:self.given_names.find(' ')])
+
+
+class University (User):
     name = models.CharField(max_length=100)
-    is_autorised = models.BooleanField(default=False)
+    short_name = models.CharField(max_length=100)
+    is_authorised = models.BooleanField(default=False)
     autorisation_expiry_date = models.DateField()
+
+    def get_full_name(self):
+        return "%s" % (self.name)
+
+    def get_short_name(self):
+        return "%s" % (self.short_name)
+
 
 class DiplomaGroup (models.Model):
     university = models.ForeignKey(University, on_delete=models.CASCADE)
@@ -27,7 +56,11 @@ class DiplomaGroup (models.Model):
     title = models.CharField(max_length=100)
     issue_date = models.DateField(default=datetime.date.today)
 
+
 class Diploma (models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     group = models.ForeignKey(DiplomaGroup, on_delete=models.CASCADE)
     diploma_file = JSONField()
+
+    def get_diploma(self):
+        return self.diploma_file
