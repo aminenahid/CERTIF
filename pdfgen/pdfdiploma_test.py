c962246e11ab27524942e6bd479e3de6bf83b2e3
lsaos <loicsaos@gmail.com>
2019-05-06 16:24:16 +0200

    PDF generation

---
+++ pdfgen/pdfdiploma_test.py
@@ -0,0 +1,13 @@
+# PDF diploma generation tests
+
+#
+# Imports
+#
+
+from pdfdiploma import pdf_diploma_from_file_to_file
+
+#
+# Main (for tests)
+#
+
+pdf_diploma_from_file_to_file('louis_inge_published.json', 'test.pdf')
