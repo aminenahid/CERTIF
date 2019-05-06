0e362ff206865e8a9015fe547e3fb00b15d54b3d
Meezlendor <louis.ohl@insa-lyon.fr>
2019-05-06 17:08:39 +0200

    FIX : new version for the blockert part

--- blockcert/UploadAndVerify.py
+++ blockcert/UploadAndVerify.py
@@ -2,6 +2,7 @@
 ######################NORMAL IMPORT
 import json
 import os
+import logging
 from io import StringIO
 
 ######################BLOCKCERTS IMPORT
@@ -14,18 +15,21 @@ PATH=os.path.realpath(__file__)
 DIR=PATH[:PATH.rfind(os.sep)]
 UPLOADFILE="toUpload{}.json"
 VERIFYFILE="toVerify.json"
-fileNumber=0
+startNumber=1
+fileNumber=1
 config.PATH=DIR
 #We need to write the private key in the appropriate file
 parsedConfig = None
 
 def _getConfig():
+    logging.info("Getting configuration for blockcert-issuer")
     global parsedConfig
     if parsedConfig is None:
         try:
             configTemp=config.get_config()
             parsedConfig=configTemp
         except Exception as error:
+            logging.error("Didn't succeed in retrieving the configuration for blockcert-issuer : %s", str(error))
             return None
     return parsedConfig
 
@@ -36,12 +40,15 @@ def addSchema(schema):
     @return True if the file was added successfully.
     """
     global fileNumber
-    fileNumber+=1
     parsedConfig=_getConfig()
-    if parsedConfig is None:
-        return False
-    with open(os.path.join(parsedConfig.unsigned_certificates_dir,UPLOADFILE.format(fileNumber)), "w+") as certificateFile:
-        certificateFile.write(schema)
+    unsignedPath=os.path.join(parsedConfig.unsigned_certificates_dir,UPLOADFILE.format(fileNumber))
+    try:
+        with open(unsignedPath, "w+") as certificateFile:
+            certificateFile.write(schema)
+            fileNumber+=1
+            logging.info("Wrote the certificate to the following path : %s", unsignedPath)
+    except:
+        logging.error("Couldn't write the certificate to the following path : %s", unsignedPath)
     return True
 
 def issueToBlockChain(pubKey, privKey):
@@ -51,7 +58,6 @@ def issueToBlockChain(pubKey, privKey):
     @param schema (json) : the json containing the cert-schema to issue on the blockchain
     @return a tuple containing first hand a boolean indicating whether the issue was successful or not, and second hand the transaction id of the issue if successful. It also provides the fully uploaded (i.e. real) schema.
        """
-    os.system('sed -i.bak "s/address[=][a-zA-Z0-9]*/address={}/g" {}'.format(pubKey, os.path.join(DIR,"conf.ini")))
     parsedConfig=_getConfig()
     if parsedConfig is None:
         return False, "", ""
@@ -59,41 +65,33 @@ def issueToBlockChain(pubKey, privKey):
 
         with open(os.path.join(parsedConfig.usb_name,parsedConfig.key_file),"w+") as privKeyFile:
             privKeyFile.write(privKey)
-        #Now, let's write the cert-schema in file as pointed out by the conf.ini file
-        #We can now set the address of the issuer
+        
         parsedConfig.issuing_address=pubKey
-        #Let's go
-        #txID = issue_certificates.main(parsedConfig)
-        #print("Issued, txId is : "+str(txID))
+        logging.info("Ready to issue")
         result=os.system("cert-issuer -c "+os.path.join(DIR,"conf.ini"))
-        #Of course, remove the useless files
-        os.remove(os.path.join(parsedConfig.usb_name,parsedConfig.key_file))
         os.system("rm "+os.path.join(parsedConfig.unsigned_certificates_dir,UPLOADFILE.format("*")))
         #Now, let's return the real signed file
-        finalFile=""
-        with open(os.path.join(parsedConfig.blockchain_certificates_dir,UPLOADFILE.format(fileNumber)), "r") as uploaded:
-               finalFile=uploaded.read()
+        finalFiles=[]
+        blockchainPath=os.path.join(parsedConfig.blockchain_certificates_dir,UPLOADFILE)
+        if result==0:
+            logging.info("Files were successfully published to the blokchain.\n You may find them at : %s ", blockchainPath.format("[{}-{}]".format(startNumber, fileNumber)))
+        for x in range(startNumber, fileNumber):
+            with open(os.path.join(parsedConfig.blockchain_certificates_dir,UPLOADFILE.format(x)), "r") as uploaded:
+                finalFile.append(uploaded.read())
         return result==0, "", finalFile
     except Exception as error:
-        print(error)
+        logging.error(error)
         return False, "", None
-
-def verifyOnBlockChain(jsonContent, transaction_id=None):
-    certificate_json=json.loads(jsonContent.decode('utf-8'))
-    certificate_model = ccore.to_certificate_model(certificate_json=certificate_json,
-                                                    txid=transaction_id,
-                                                    certificate_bytes=jsonFile)
-    result = verifier.verify_certificate(certificate_model, options)
-    return result
 
 def verifyOnBlockChain_v2(schema, transaction_id=None):
     try:
         with open(VERIFYFILE,"w+") as toVerify:
             toVerify.write(json.dumps(schema))
     except Exception as error:
-            print(error)
+        logging.error(error)
+    logging.info("Starting verification")
     res = verifier.verify_certificate_file(VERIFYFILE, transaction_id)
-    #Now, let's check if everything is passed !
     passed= (len(res)==len([x for x in res if "passed" in x["status"]]))
     os.remove(VERIFYFILE)
+    logging.info("Verification done")
     return passed
